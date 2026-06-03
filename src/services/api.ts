c69029acc97fa5d6b56c12e7ce96f0adff9d0612
nkanusephora00-droid <nkanusephora00@gmail.com>
2026-06-03 13:37:45 +0100

    Add files via upload

--- src/services/api.ts
+++ src/services/api.ts
@@ -103,8 +103,8 @@ export interface TestSession {
   applicationNom?: string;
   environnement?: string;
   version?: string;
-  nom_document?: string;
-  date_creation: string;
+  nomDocument?: string;
+  dateCreation: string;
   statut: string;
   role?: string;
   createdBy?: number;
